fa0f4cdb84e728c332213a9d00111283db0d0809
talita <talitamsx@gmail.com>
2025-07-24 21:16:09 -0300

    first commit

---
+++ src/main/java/br/com/bitrama/CadastroDePersonagens/CadastroDePersonagensApplication.java
@@ -0,0 +1,13 @@
+package br.com.bitrama.CadastroDePersonagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDePersonagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDePersonagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bitrama/CadastroDePersonagens/CadastroDePersonagensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bitrama.CadastroDePersonagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDePersonagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
